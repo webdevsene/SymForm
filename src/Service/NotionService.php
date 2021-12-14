841e7d7d47e9ed2c1ac43811facb9f7b054f3551
webdevsene <dt.master.ws@gmail.com>
2021-12-14 14:26:17 +0000

    footer personlized

--- src/Service/NotionService.php
+++ src/Service/NotionService.php
@@ -55,7 +55,7 @@ class NotionService
         return $this->formatData($this->makeRequest($array));
     }
 
-    // Format les données
+    // Formater les données
     public function formatData(array $data): array
     {
         $formattedData = [];
